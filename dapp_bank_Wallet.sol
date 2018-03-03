840bf325a97053d9b178743b3f617132d017d92a
bsKannan <36512419+bsKannan@users.noreply.github.com>
2018-03-03 13:14:00 +0530

    Update dapp_bank_Wallet.sol

--- dapp_bank_Wallet.sol
+++ dapp_bank_Wallet.sol
@@ -2,43 +2,43 @@
 pragma solidity ^0.4.0;
 contract Bank_Wallet
 {
-    struct bk
+    struct bks
     { 
         uint256 balance;   
      }
-    mapping(address => bk)bk1;
-    mapping(address=>mapping(address => uint256))bk2;
+    mapping(address => bks)bks1;
+    mapping(address=>mapping(address => uint256))bks2;
     address owner=msg.sender;
    
    function totalSupply(uint256 dep)
     {
-        bk1[owner].balance=dep;
+        bks1[owner].balance=dep;
     }
     function transfer(address reciver,uint256 transamt) public
     {
-        require(bk1[msg.sender].balance>transamt);
-        bk1[reciver].balance+=transamt;
-        bk1[msg.sender].balance-=transamt;
+        require(bks1[msg.sender].balance>transamt);
+        bks1[reciver].balance+=transamt;
+        bks1[msg.sender].balance-=transamt;
     }
      function approve(address _spender, uint256 _value) returns (bool success) {
         
-        bk2[msg.sender][_spender] = _value;
+        bks2[msg.sender][_spender] = _value;
         
         return true;
     }
     
    function allowance(address _owner, address _spender) constant returns (uint256 remaining) 
    {
-      return bk2[_owner][_spender];
+      return bks2[_owner][_spender];
    }
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) 
     {
-       if (bk2[msg.sender][_from] >= _value && _value > 0) 
+       if (bks2[msg.sender][_from] >= _value && _value > 0) 
        {
-           bk1[_to].balance += _value;
-            bk1[msg.sender].balance -= _value;
-            bk2[msg.sender][_from] -= _value;
+           bks1[_to].balance += _value;
+            bks1[msg.sender].balance -= _value;
+            bks2[msg.sender][_from] -= _value;
             
             return true;
         } 
@@ -49,6 +49,6 @@ contract Bank_Wallet
     }
     function balanceOf(address balcheck)public view returns(uint256)
     {
-        return bk1[balcheck].balance;
+        return bks1[balcheck].balance;
     }
 }
